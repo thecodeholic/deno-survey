89b17f26e3959009e9d0abdd6b570bc072913797
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-02 10:05:49 +0400

    Create router.ts and move routes there

---
+++ router.ts
@@ -0,0 +1,38 @@
+import { Router } from "./deps.ts";
+import siteController from "./controllers/SiteController.ts";
+import surveyController from "./controllers/SurveyController.ts";
+import questionController from "./controllers/QuestionController.ts";
+import apiController from "./controllers/ApiController.ts";
+
+
+const router = new Router();
+
+router
+  .get('/', siteController.surveys)
+  .get('/survey/:id', siteController.viewSurvey)
+
+  .post('/api/register', apiController.register)
+  .post('/api/login', apiController.login)
+
+  // Survey CRUD
+  .get('/api/survey', surveyController.getAll)
+  .get('/api/survey/:id', surveyController.getSingle)
+  .post('/api/survey', surveyController.create)
+  .put('/api/survey/:id', surveyController.update)
+  .delete('/api/survey/:id', surveyController.delete)
+
+  // Survey Question CRUD
+  .get('/api/survey/:surveyId/question', questionController.getBySurvey)
+  .get('/api/survey/:surveyId/question/:id', questionController.getSingle)
+  .post('/api/survey/:surveyId/question', questionController.create)
+  .put('/api/survey/:surveyId/question/:id', questionController.update)
+  .delete('/api/survey/:surveyId/question/:id', questionController.delete)
+
+  // Survey Question Shorter URLs
+  .get('/api/question/:surveyId', questionController.getBySurvey)
+  .get('/api/question/:id', questionController.getSingle)
+  .post('/api/question/:surveyId', questionController.create)
+  .put('/api/question/:id', questionController.update)
+  .delete('/api/question/:id', questionController.delete)
+
+export default router;
