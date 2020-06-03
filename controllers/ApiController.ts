8542343443f6c23d3dc7c1304f2e0470968f00a3
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-03 08:39:42 +0400

    Implement login using JWT

--- controllers/ApiController.ts
+++ controllers/ApiController.ts
@@ -1,9 +1,12 @@
-import { RouterContext } from "../deps.ts";
+import { RouterContext, makeJwt, Jose, Payload, setExpiration } from "../deps.ts";
 import { userCollection } from "../mongo.ts";
 import { hashSync, compareSync } from "../deps.ts";
 import User from "../models/User.ts";
 
-
+const header: Jose = {
+  alg: 'HS256',
+  typ: 'JWT'
+};
 export class ApiController {
   async register(ctx: RouterContext) {
     const { value: { name, email, password } } = await ctx.request.body();
@@ -21,8 +24,30 @@ export class ApiController {
     ctx.response.body = user;
   }
   async login(ctx: RouterContext) {
-    const { value } = await ctx.request.body();
-    console.log(value);
+    const { value: { email, password } } = await ctx.request.body();
+    let user = await User.findOne({ email });
+    if (!user) {
+      ctx.response.status = 422;
+      ctx.response.body = { message: "Incorrect email" }
+      return;
+    }
+    if (!compareSync(password, user.password)) {
+      ctx.response.status = 422;
+      ctx.response.body = { message: "Incorrect password" }
+      return;
+    }
+
+    const payload: Payload = {
+      iss: user.email,
+      exp: setExpiration(Date.now() + parseInt(Deno.env.get('JWT_EXP_DURATION') || '0'))
+    }
+    const jwt = makeJwt({ key: Deno.env.get('JWT_SECRET_KEY') || '', payload, header })
+
+    ctx.response.body = {
+      success: true,
+      user,
+      jwt
+    };
   }
 }
 
--- deps.ts
+++ deps.ts
@@ -2,3 +2,4 @@ export { Application, Router, RouterContext } from "https://deno.land/x/oak@v5.0
 export { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 export {hashSync, compareSync} from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
 import "https://deno.land/x/dotenv@v0.4.1/load.ts";
+export { makeJwt, setExpiration, Jose, Payload } from "https://deno.land/x/djwt/create.ts"
