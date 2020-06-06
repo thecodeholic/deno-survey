fb583bd5fe8e70a6ba1a7b4261a0e7cc9977a9b4
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-06 15:16:27 +0400

    Rename public folder into assets

--- middleware/staticFileMiddleware.ts
+++ middleware/staticFileMiddleware.ts
@@ -2,10 +2,10 @@ import {send, Context} from "../deps.ts";
 import {fileExists} from "../helpers.ts";
 
 export const staticFileMiddleware = async (context: Context, next: Function) => {
-  const path = `${Deno.cwd()}/public${context.request.url.pathname}`;
+  const path = `${Deno.cwd()}/assets${context.request.url.pathname}`;
   if (await fileExists(path)) {
     await send(context, context.request.url.pathname, {
-      root: `${Deno.cwd()}/public`,
+      root: `${Deno.cwd()}/assets`,
     });
   } else {
     await next();
