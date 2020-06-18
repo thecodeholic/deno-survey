2a573c32a7f4fc29e16db7ecfa443c83ba2547fd
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-06-18 09:39:04 +0400

    Update returned user on registrtion

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -27,7 +27,11 @@ export class AuthController {
     user = new User({ name, email, password: hashedPassword });
     await user.save();
     ctx.response.status = 201;
-    ctx.response.body = user;
+    ctx.response.body = {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    };
   }
   async login(ctx: RouterContext) {
     const { value: { email, password } } = await ctx.request.body();
